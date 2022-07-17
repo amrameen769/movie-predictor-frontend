bb5fb91afa8ff7eefc9fa39d8f9b7e5f7abbddbc
AMR Solutions LTD <amrorg333@gmail.com>
2022-07-18 00:33:47 +0530

    Welcome Screen Skeleton created.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import MpLogo from '../public/mp_logo_trans.png'
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,12 +8,20 @@ export default function Home() {
       <Head>
         <title>Movie Predictor</title>
         <meta name="description" content="Movies you would like to watch!" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/mp_logo.png" />
       </Head>
 
       <main className={"container mx-auto"}>
+          <div className={"flex flex-col items-center"}>
           <div className="flex items-center justify-center min-h-screen">
-              <h1 className={""}>Welcome to Movie-Predictor</h1>
+              <Image src={MpLogo} objectFit={"contain"}/>
+              <h1 className={"font-thin text-6xl min-w-min"}>Welcome to Movie Predictor</h1>
+          </div>
+          <div className={"flex-1 -m-60"}>
+              <button className={"border-0 h-14 w-36 p-2 rounded-xl bg-black text-white font-semibold hover:border-2 hover:bg-white hover:border-gray-600 hover:text-black transition-all duration-400 ease-out"}>
+                  Learn More...
+              </button>
+          </div>
           </div>
       </main>
     </div>
