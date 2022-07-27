fafa85faa8c4da11393dea252bc0a7b46fa670f2
mr-dark123 <sreerag123438@gmail.com>
2022-07-27 14:00:55 +0530

    Rows Boilerplate added

--- components/Row.js
+++ components/Row.js
@@ -1,5 +1,13 @@
-<div className="bg-white ml-5">
-  <div className="flex overflow-y-hidden overflow-x-scroll p-10">
-    <img src="https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_FMjpg_UX1000_.jpg" />
+
+function Row(){
+  return(
+<div className="bg-white">
+  <div className="flex overflow-y-hidden overflow-x-scroll p-10 h-max">
+    {[...new Array(100)].map((data, index) => (
+    <img key={index} className="banner-row" src="https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_FMjpg_UX1000_.jpg" />
+    ))}
   </div>
-</div>;
+</div>
+);
+}
+export default Row;
